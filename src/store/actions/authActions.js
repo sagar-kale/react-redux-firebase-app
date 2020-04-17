03a404e2aa4c110bd45a89a989a193b9cfe2bf82
sagar-kale <sagark.kale@hotmail.com>
2020-04-17 16:22:28 +0530

    setting last seen logout

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -54,10 +54,12 @@ export const logOut = () => {
     return async (dispatch, getState) => {
         const state = getState();
         const uid = state.firebase.profile.uid;
-    //    console.log('current uid', uid);
+        //    console.log('current uid', uid);
         const userRef = firebase.database().ref(`presence/${uid}`);
-        firebase.database().ref(`presence/${uid}/lastOnline`).set(firebase.database.ServerValue.TIMESTAMP);
         userRef.set({ status: '☆ offline' });
+        const lastSeenRef = firebase.database().ref(`presence/${uid}/lastOnline`);
+        lastSeenRef.set(firebase.database.ServerValue.TIMESTAMP);
+
         await firebase.auth().signOut();
 
         dispatch({ type: actionTypes.CLEAR_DATA, preserve: { ordered: ['projects'] } })
