d83bab2f042542ad56647aaa07e50be160f04b6e
sagar-kale <sagark.kale@hotmail.com>
2020-04-20 20:43:00 +0530

    resetting iphone ui to default

--- src/components/dashboard/UserNotification.js
+++ src/components/dashboard/UserNotification.js
@@ -5,7 +5,7 @@ import { useFirestoreConnect } from 'react-redux-firebase';
 import { Link, withRouter } from 'react-router-dom';
 import { userNotificationsQuery } from '../../queries/queries';
 import { markNotificationRead } from '../../store/actions/productActions';
-import { getPath, isSafari } from '../../utils/helpers';
+import { getPath } from '../../utils/helpers';
 
 const UserNotification = withRouter((props) => {
 
@@ -13,9 +13,8 @@ const UserNotification = withRouter((props) => {
     const dispatch = useDispatch();
 
     useFirestoreConnect(userNotificationsQuery(user.handle));
-    //const error = useSelector(state => state.firestore.errors.byQuery[getQueryName(userNotificationsQuery(user.handle))]);
+    // const error = useSelector(state => state.firestore.errors.byQuery[getQueryName(userNotificationsQuery(user.handle))]);
     const notifications = useSelector(state => state.firestore.ordered.user_notifications);
-
 
     const handleClick = (e) => {
         let notificationIds = newNotifications(notifications);
@@ -30,41 +29,20 @@ const UserNotification = withRouter((props) => {
         }
     }, [props, notifications]);
 
-    const safari = (notification) => {
-        return (
-            <a href={`post/${notification.postId}`} onClick={handleClick}>
-
-                <small className={`tiny material-icons left-align ${notification.read === false ? 'orange-text' : 'cyan-text'}`}>{isLike(notification.type) ? 'favorite' : 'chat'}</small>
-                <small className="pink-text"> {notification.sender} </small>
-                <small>{isLike(notification.type) ? 'liked' : 'commented on'} your post</small>
-                <small className="grey-text note-date">
-                    ( {moment(notification.createdAt.toDate()).fromNow()})
-                            </small>
-            </a>
-        )
-    }
-
-    const other = (notification) => {
-        return (
-            <Link to={`${getPath(props.location.pathname)}/${notification.postId}`} onClick={handleClick}>
-
-                <small className={`tiny material-icons left-align ${notification.read === false ? 'orange-text' : 'cyan-text'}`}>{isLike(notification.type) ? 'favorite' : 'chat'}</small>
-                <small className="pink-text"> {notification.sender} </small>
-                <small>{isLike(notification.type) ? 'liked' : 'commented on'} your post</small>
-                <small className="grey-text note-date">
-                    ( {moment(notification.createdAt.toDate()).fromNow()})
-                            </small>
-            </Link>
-        )
-    }
-
-
     return (
         <div>
             {notifications && notifications.length > 0 ? notifications.map(notification => {
                 return (
                     <li key={notification.id}>
-                        {isSafari() === true ? safari(notification) : other(notification)}
+
+                        <Link to={`${getPath(props.location.pathname)}/${notification.postId}`} onClick={handleClick}>
+
+                            <small className={`tiny material-icons left-align ${notification.read === false ? 'orange-text' : 'cyan-text'}`}>{isLike(notification.type) ? 'favorite' : 'chat'}</small>
+                            <small className="pink-text"> {notification.sender} </small>
+                            <small>{isLike(notification.type) ? 'liked' : 'commented on'} your post</small>
+                            <small className="grey-text note-date">
+                                ( {moment(notification.createdAt.toDate()).fromNow()})</small>
+                        </Link>
                     </li>
 
                 )
