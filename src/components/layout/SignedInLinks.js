31b0c23a7abfc85a18f345b1f6499cbdfd444e28
sagar-kale <sagark.kale@hotmail.com>
2021-05-09 15:17:42 +0530

    img pah corrected

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -7,6 +7,8 @@ import { logOut } from '../../store/actions/authActions';
 import { isSafari, newNotifications } from '../../utils/helpers';
 import UserNotification from '../dashboard/UserNotification';
 import { UserStatus } from './UserStatus';
+import officeImg from '../../img/office.jpg';
+import picLogo from '../../img/yuna.jpg';
 
 const SignedInLinks = (props) => {
 
@@ -54,9 +56,9 @@ const SignedInLinks = (props) => {
             <ul id="slide-out" className="sidenav">
                 <li><div className="user-view">
                     <div className="background">
-                        <img src={require('../../img/office.jpg')} alt="office" />
+                        <img src={officeImg} alt="office" />
                     </div>
-                    <img className="circle" src={user.photoURL || require('../..//img/yuna.jpg')} alt="yuna" />
+                    <img className="circle" src={user.photoURL || picLogo} alt="yuna" />
                     <span className="white-text name">{user.firstName} {user.lastName}</span>
                     <span className="white-text email">{user.email}</span>
                 </div></li>
